f1f5a73720070d16ec296c73a836203e58f87ae9
LMTrefry <Luke.M.Trefry@liosn.enc.edu>
2020-09-19 15:18:16 -0400

    Waypoints
    Added several waypoints (23 in total) which the ball bounces between randomly.

---
+++ Assets/Scripts/Waypoint.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Waypoint : MonoBehaviour
+{
+
+    public Transform[] waypoints; //array of waypoints
+    Vector3 targetPoint;
+
+    public float speed = 1;
+
+    public float distanceThreshold;
+
+    void Start()
+    {
+        targetPoint = waypoints[0].position;
+    }
+
+    
+    void Update()
+    {
+        if(Vector3.Distance(transform.position, targetPoint) < distanceThreshold)
+        {
+            changeTarget();
+        }
+        moveTowards();
+    }
+
+    void moveTowards()
+    {
+        transform.position = Vector3.MoveTowards(transform.position, targetPoint, Time.deltaTime * speed);
+    }
+
+    void changeTarget()
+    {
+        int randomIndex = Random.Range(0, waypoints.Length);
+        targetPoint = waypoints[randomIndex].position;
+    }
+
+}
